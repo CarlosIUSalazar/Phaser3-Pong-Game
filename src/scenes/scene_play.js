7c388c2cd5624f6d6ec69db30276b57d467041b2
Carlos Salazar <carlosiusalazar@gmail.com>
2020-04-05 18:11:09 +0900

    Set up Phaser files

---
+++ src/scenes/scene_play.js
@@ -0,0 +1,8 @@
+class Scene_play extends Phaser.Scene {
+    constructor() {
+        super({key: "Scene_play"})
+    }
+    preload(){
+        console.log("la escena play se ha cargado")
+    }
+}
